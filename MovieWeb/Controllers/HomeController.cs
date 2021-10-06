7011917bfa21e2c624715417c47708697cabfd50
Josefine Paaske <1086237@ucn.dk>
2021-10-06 11:20:50 +0200

    Add project files.

---
+++ MovieWeb/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieDAL;
+using MovieDAL.Daos;
+using MovieDAL.Model;
+using MovieWeb.Models;
+using System.Diagnostics;
+
+namespace MovieWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDao<Movie> _movieDao;
+        //private readonly IDao<Movie> _movieDao= new MovieDao(); // This is BAD!!! High coupling and all that
+
+
+        public HomeController(ILogger<HomeController> logger, IDao<Movie>movieDao) //kan ikke resolves IDao<Movie>movieDao
+        {
+            _logger = logger;
+            _movieDao = movieDao;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _movieDao.GetAll();
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieWeb/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MovieDAL;
+using MovieDAL.Daos;
+using MovieDAL.Model;
+
+namespace MovieWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDao<Movie>>(s => DaoFactory.Create<Movie>()); //Dao er sat op som service, der kan dependency injektes
+                                                                              //s er bare en variabel i lambda funktionen
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
